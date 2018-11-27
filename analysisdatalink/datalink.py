09a3214984b0ab4c186f166a176625dc9aa77242
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-11-27 15:34:24 -0500

    adapted changes from csm_v0 hierarchical abstraction of database access: datalink_base: low level - never meant to be accessed directly datalink: abstracts direct SQL access but is generic to table specific queries datalink_ext: implements schema specific queries

--- analysisdatalink/datalink.py
+++ analysisdatalink/datalink.py
@@ -1,234 +1,62 @@
-from collections import defaultdict
-from shapely import wkb
-import geopandas as gpd
-from emannotationschemas.base import flatten_dict
-from emannotationschemas import get_schema
-from emannotationschemas.models import make_annotation_model, make_cell_segment_model
-from emannotationschemas.mesh_models import make_post_synaptic_compartment_model
-from geoalchemy2.shape import to_shape, from_shape
-from geoalchemy2 import func as geo_func
-from geoalchemy2.elements import WKBElement
-from shapely import geometry
-from sqlalchemy import func
-from sqlalchemy import and_
-import neuroglancer
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import time
 import numpy as np
-import pandas as pd
-import os
-from collections import defaultdict, Iterable
 
-def wkb_to_numpy(wkb):
-    shp=to_shape(wkb)
-    return np.array([shp.xy[0][0],shp.xy[1][0], shp.z], dtype=np.int)
+import sqlalchemy
+from analysisdatalink import datalink_base
 
-def fix_wkb_columns(df):
-    if len(df) > 0:
-        for colname in df.columns:
-            if isinstance(df.at[0,colname], WKBElement):
-                df[colname] = df[colname].apply(wkb_to_numpy)
-    return df
+class AnalysisDataLink(datalink_base.AnalysisDataLinkBase):
+    def __init__(self, dataset_name, materialization_version,
+                 sqlalchemy_database_uri=None):
+        super().__init__(dataset_name, materialization_version,
+                         sqlalchemy_database_uri)
 
-def pre_post_dict():
-    return {'post': [], 'pre': []}
+    def specific_query(self, tables, filter_in_dict={}, filter_notin_dict={},
+                       select_columns=None):
+        """ Allows a more narrow query without requiring knowledge about the
+            underlying data structures
 
-class AnnotationDataLink():
-    def __init__(self, dataset, version, database_uri=None, session=None):
-        self.database_uri = database_uri
-        self.data_version = version
-        self.dataset = dataset
-        self.session = session
-        self.models = {}
-
-    def add_annotation_model(self, schema_name, table_name, model_name=None, version=None):
-        """Loads database model for an annotation schema
-
-        Args:
-            schema_name: Name of the schema for the table from emannotationschemas 
-            table_name: Table name for the database
-            model_name: Reference name for model (optional, defaults to same as table_name)
-            version: Materialization version (Optional)
+        :param tables: list of lists
+            standard: list of one entry: table_name of table that one wants to
+                      query
+            join: list of two lists: first entries are table names, second
+                                     entries are the columns used for the join
+        :param filter_in_dict: dict of dicts
+            outer layer: keys are table names
+            inner layer: keys are column names, values are entries to filter by
+        :param select_columns: list of str
+        :return:
         """
-        if model_name in self.models:
-            print('Model name \'{}\' already exists'.format(model_name))
-        if version is None:
-            version = self.data_version
-        self.models[model_name] = make_annotation_model(self.dataset, schema_name, table_name, version=version)
+        tables = [[table] if not isinstance(table, list) else table
+                  for table in tables]
+        query_args = [self.model(table[0]) for table in tables]
 
-    def make_query(self, query_args, join_args=None, filter_args=None):
-        """Constructs a query object with selects, joins, and filters
-
-        Args:
-            query_args: Iterable of objects to query
-            join_args: Iterable of objects to set as a join (optional)
-            filter_args: Iterable of iterables
-
-        Returns:
-            SQLAchemy query object
-        """
-        query = self.session.query(*query_args)
-        if join_args is not None:
-            query = query.join(*join_args)
-        if filter_args is not None:
-            for f in filter_args:
-                query = query.filter(*f)
-        return query
-
-    def dataframe_from_query(self, query, fix_wkb=True):
-        """ Query the database and make a dataframe out of the results
-
-        Args:
-            query: SQLAlchemy query object
-            fix_wkb: Boolean to turn wkb objects into numpy arrays (optional, default is True)
-        
-        Returns:
-            Dataframe with query results
-        """
-        df = pd.read_sql(query.statement, self.session.bind, coerce_float=False)
-        if fix_wkb:
-            df = fix_wkb_columns(df)
-        return df
-
-
-class CellTypeDataLink(AnnotationDataLink):
-    def __init__(self, dataset, version, session):
-        super(CellTypeDataLink, self).__init__(dataset, version, session=session)
-
-    def add_model(self, table_name, schema_name='cell_type_local', model_name=None, version=None):
-        self.add_annotation_model(schema_name, table_name, model_name=model_name, version=version)
-
-    def query_cell_type(self, model_name, oids=None, cell_types=None):
-        query_args = (self.models[model_name],)
-        filter_args = []
-        if oids is not None:
-            filter_args.append( (self.models[model_name].pt_root_ids.in_(oids)) )
-        if cell_types is not None:
-            if isinstance(cell_types, str):
-                filter_args.append((self.models[model_name].cell_type == cell_types,))
-            elif isinstance(cell_types, Iterable):
-                filter_args.append((self.models[model_name].cell_type.in_(cell_types),))
-        qry = self.make_query(query_args=query_args, filter_args=filter_args)
-        data_df = self.dataframe_from_query(qry)
-        return data_df
-
-
-class CellIdDataLink(AnnotationDataLink):
-    def __init__(self, dataset, version, session):
-        super(CellIdDataLink, self).__init__(dataset, version, session=session)
-
-    def add_model(self, table_name, schema_name='microns_func_coreg', model_name=None, version=None):
-        self.add_annotation_model(schema_name, table_name, model_name=model_name, version=version)
-
-    def query_cell_ids(self, model_name, oids=None, cell_ids=None):
-        query_args = (self.models[model_name],)
-        filter_args = []
-        if oids is not None:
-            filter_args.append( (self.models[model_name].pt_root_ids.in_(oids)) )
-        if cell_ids is not None:
-            if isinstance(cell_ids, int):
-                filter_args.append((self.models[model_name].func_id == cell_ids,))
-            elif isinstance(cell_ids, Iterable):
-                filter_args.append((self.models[model_name].func_id.in_(cell_ids),))
-
-        qry = self.make_query(query_args=query_args, filter_args=filter_args)
-        data_df = self.dataframe_from_query(qry)
-        return data_df
-
-
-class SynapseDataLink(AnnotationDataLink):
-    def __init__(self, dataset, version, session):
-        super(SynapseDataLink, self).__init__(dataset, version, session=session)
-        self.compartment_models = defaultdict(pre_post_dict)
-
-    def add_synapse_model(self, table_name, schema_name='synapse', model_name=None, version=None, set_compartments=False):
-        self.add_annotation_model(schema_name, table_name, model_name=model_name, version=version)
-        if set_compartments:
-            if model_name is None:
-                model_name = table_name
-            try:
-                self.add_postsynaptic_compartments(table_name, synapse_model_name=model_name, model_name=None)
-            except:
-                print('Could not add postsynaptic compartments')
-
-    def add_postsynaptic_compartments(self, synapse_table, synapse_model_name=None, model_name=None):
-        if (synapse_model_name is None) and (len(self.models) == 1):
-            synapse_model_name = list(self.models.keys()).pop()            
-        if model_name is None:
-            model_name = '{}_post_compartments'.format(synapse_model_name)
-        if model_name in self.models:
-            raise ValueError('Model with that name already exists')    
-        self.models[model_name] = make_post_synaptic_compartment_model(self.dataset, synapse_table, self.data_version)
-        self.compartment_models[synapse_model_name]['post'].append( model_name )
-
-    def query_synapses(self,
-                       synapse_model_name=None,
-                       pre_ids=None,
-                       post_ids=None,
-                       post_compartments=None,
-                       return_post_compartments=True,
-                       post_compartment_model_name=None):
-        if synapse_model_name is None:
-            all_models = set(self.models.keys())
-            comp_models = []
-            for syn_m in self.compartment_models.values():
-                for m in syn_m['post']:
-                    comp_models.append(m)
-            comp_models = set(comp_models)
-            syn_models = all_models.difference(comp_models)
-            if len(syn_models)==1:
-                synapse_model_name = syn_models.pop()
-            else:
-                print('Please specify which synapse model to use')
-                return
-
-        if pre_ids is None and post_ids is None and post_compartments is None:
-            print('\tNo filters specified. This could take a while...')
-        if post_compartments is not None:
-            return_post_compartments = True
-        if return_post_compartments:
-            if (post_compartment_model_name is None) and (len(self.compartment_models[synapse_model_name]['post']) == 1):
-                post_compartment_model_name = self.compartment_models[synapse_model_name]['post'][0]
-
-        if return_post_compartments:
-            query_args = (self.models[post_compartment_model_name], self.models[synapse_model_name])
-            join_args = (self.models[synapse_model_name], self.models[synapse_model_name].id == self.models[post_compartment_model_name].synapse_id)
+        if len(tables) == 2:
+            join_args = (self.model(tables[1][0]),
+                         self.model(tables[1][0]).__dict__[tables[1][1]] ==
+                         self.model(tables[0][0]).__dict__[tables[0][1]])
+        elif len(tables) > 2:
+            raise Exception("Currently, only single joins are supported")
         else:
-            query_args = (self.models[synapse_model_name], )
             join_args = None
 
         filter_args = []
-        if pre_ids is not None:
-            filter_args.append( (self.models[synapse_model_name].pre_pt_root_id.in_(pre_ids),) )
-        if post_ids is not None:
-            filter_args.append( (self.models[synapse_model_name].post_pt_root_id.in_(post_ids),) )
-        if post_compartments is not None:
-            filter_args.append( (self.models[post_compartment_model_name].label.in_(post_compartments),))
 
-        qry = self.make_query(query_args, join_args, filter_args)
-        data_df = self.dataframe_from_query(qry)
+        for filter_table, filter_table_dict in filter_in_dict.items():
+            for column_name in filter_table_dict.keys():
+                filter_values = filter_table_dict[column_name]
+                filter_values = np.array(filter_values, dtype="O")
 
-        return data_df
+                filter_args.append((self.model(filter_table).__dict__[column_name].
+                                    in_(filter_values), ))
+
+        for filter_table, filter_table_dict in filter_notin_dict.items():
+            for column_name in filter_table_dict.keys():
+                filter_values = filter_table_dict[column_name]
+                filter_values = np.array(filter_values, dtype="O")
+
+                filter_args.append((sqlalchemy.not_(self.model(filter_table).__dict__[column_name].
+                                                    in_(filter_values)), ))
+
+        return self._query(query_args=query_args, filter_args=filter_args,
+                           join_args=join_args, select_columns=select_columns)
 
 
-class DataLink(AnnotationDataLink):
-    def __init__(self, dataset, version, database_uri=None):
-        super(DataLink, self).__init__(dataset, version, database_uri=database_uri)        
-        if database_uri is None:
-            database_uri = os.getenv('DATABASE_URI')
-            assert database_uri is not None
-        self.database_uri = database_uri
-
-        engine = create_engine(self.database_uri, echo=False)
-        Session = sessionmaker(bind=engine)
-        self.session = Session()
-
-        self.dataset = dataset
-        self.data_version = version        
-        make_cell_segment_model(self.dataset, version=self.data_version)
-
-        self.synapses = SynapseDataLink(self.dataset, self.data_version, self.session)
-        self.cell_types = CellTypeDataLink(self.dataset, self.data_version, self.session)
-        self.cell_ids = CellIdDataLink(self.dataset, self.data_version, self.session)
-        
--- analysisdatalink/datalink_base.py
+++ analysisdatalink/datalink_base.py
@@ -1,120 +1,223 @@
-from collections import defaultdict
-from shapely import wkb
-import geopandas as gpd
-from emannotationschemas.base import flatten_dict
-from emannotationschemas import get_schema
-from emannotationschemas.models import make_annotation_model, make_cell_segment_model
+from emannotationschemas import models as em_models, \
+    mesh_models as em_mesh_models
 from geoalchemy2.shape import to_shape, from_shape
-from geoalchemy2 import func as geo_func
 from geoalchemy2.elements import WKBElement
-from shapely import geometry
-from sqlalchemy import func
-from sqlalchemy import and_
-import neuroglancer
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import time
 import numpy as np
 import pandas as pd
 import os
+import json
+import requests
+
+import analysisdatalink
+
 
 def wkb_to_numpy(wkb):
+    """ Fixes single geometry column """
     shp=to_shape(wkb)
     return np.array([shp.xy[0][0],shp.xy[1][0], shp.z], dtype=np.int)
 
+
 def fix_wkb_columns(df):
+    """ Fixes geometry columns """
     if len(df) > 0:
         for colname in df.columns:
             if isinstance(df.at[0,colname], WKBElement):
                 df[colname] = df[colname].apply(wkb_to_numpy)
     return df
 
-class DataLink():
-    def __init__(self, dataset, version, database_uri=None):
-        if database_uri is None:
-            database_uri = os.getenv('DATABASE_URI')
-            assert database_uri is not None
-        self.database_uri = database_uri
 
-        engine = create_engine(self.database_uri, echo=False)
-        Session = sessionmaker(bind=engine)
-        self.session = Session()
-        self.dataset = dataset
-        self.data_version = version
-        self.models = {}
-        make_cell_segment_model(self.dataset, version=self.data_version)
+def get_annotation_info(dataset_name, table_name, annotation_endpoint=None):
+    """ Reads annotation info from annotation engine endpoint """
 
-    def add_annotation_model(self, model_name, schema_name, table_name, version=None):
-        if model_name in self.models:
-            print('Model name \'{}\' already exists'.format(model_name))
-        if version is None:
-            version = self.data_version
-        self.models[model_name] = make_annotation_model(self.dataset, schema_name, table_name, version=version)
+    if table_name is "postsynapsecompartment":
+        return {"schema_name": "PostSynapseCompartment"}
 
-    def run_query(self, model_name, filter_obj=None):
-        if filter_obj is None:
-            data_qry = self.session.query(self.models[model_name])
+    if annotation_endpoint is None:
+        annotation_endpoint = analysisdatalink.annotation_endpoint
+
+    url = "%s/dataset/%s/%s" % (annotation_endpoint, dataset_name, table_name)
+    r = requests.get(url)
+
+    assert r.status_code == 200
+
+    return json.loads(r.content)
+
+
+class AnalysisDataLinkBase(object):
+    def __init__(self, dataset_name, materialization_version,
+                 sqlalchemy_database_uri=None):
+
+        if sqlalchemy_database_uri is None:
+            sqlalchemy_database_uri = os.getenv('DATABASE_URI')
+            assert sqlalchemy_database_uri is not None
+
+        self._dataset_name = dataset_name
+        self._materialization_version = materialization_version
+        self._sqlalchemy_database_uri = sqlalchemy_database_uri
+        self._models = {}
+
+        self._models["cellsegment"] = em_models.make_cell_segment_model(
+            dataset_name, version=self.materialization_version)
+
+        self._sqlalchemy_engine = create_engine(sqlalchemy_database_uri,
+                                                echo=True)
+        em_models.Base.metadata.create_all(self.sqlalchemy_engine)
+
+        self._sqlalchemy_session = sessionmaker(bind=self.sqlalchemy_engine)
+
+        self._this_sqlalchemy_session = None
+
+    @property
+    def dataset_name(self):
+        return self._dataset_name
+
+    @property
+    def materialization_version(self):
+        return self._materialization_version
+
+    @property
+    def sqlalchemy_database_uri(self):
+        return self._sqlalchemy_database_uri
+
+    @property
+    def sqlalchemy_engine(self):
+        return self._sqlalchemy_engine
+
+    @property
+    def sqlalchemy_session(self):
+        return self._sqlalchemy_session
+
+    @property
+    def this_sqlalchemy_session(self):
+        if self._this_sqlalchemy_session is None:
+            self._this_sqlalchemy_session = self.sqlalchemy_session()
+        return self._this_sqlalchemy_session
+
+    def model(self, table_name, is_synapse_compartment=False):
+        """ Returns annotation model for table
+
+        :param table_name: str
+        :return: em annotation model
+        """
+        if not is_synapse_compartment and self._add_annotation_model(table_name):
+            return self._models[table_name]
+        # elif is_synapse_compartment and self._add_synapse_compartment_model():
+        #     return self.
         else:
-            data_qry = self.session.query(self.models[model_name]).filter(filter_obj)
-        data_df = pd.read_sql(data_qry.statement, self.session.bind, coerce_float=False)
-        return data_df
+            raise Exception("Could not make annotation model")
 
-    def run_query_raw(self, Model, filter_obj=None):
-        if filter_obj is None:
-            data_qry = self.session.query(Model)
-        else:
-            data_qry = self.session.query(Model).filter(filter_obj)
-        data_df = pd.read_sql(data_qry.statement, self.session.bind, coerce_float=False)
-        return data_df
+    def _add_synapse_compartment_model(self, synapse_table_name,
+                                       table_name="postsynapsecompartment"):
+        if table_name in self._models:
+            print('Model name \'{}\' already exists'.format(table_name))
+            return True
 
-    def query_synapses_by_id(self, model_name, pre_ids=None, post_ids=None):
-        if pre_ids is None and post_ids is None:
-            print('Please specify either pre or post ids')
-            return
+        try:
+            self._models[table_name] = em_mesh_models.make_post_synaptic_compartment_model(
+                dataset=self.dataset_name, synapse_table=synapse_table_name,
+                version=self.materialization_version)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-        if pre_ids is None:
-            filter_obj = self.models[model_name].post_pt_root_id.in_(post_ids)
-        elif post_ids is None:
-            filter_obj = self.models[model_name].pre_pt_root_id.in_(pre_ids)
-        else:
-            filter_obj = and_(self.models[model_name].post_pt_root_id.in_(post_ids),
-                              self.models[model_name].pre_pt_root_id.in_(pre_ids))
+    def _add_annotation_model(self, table_name):
+        """ Loads database model for an annotation schema
 
-        data_df = self.run_query(model_name, filter_obj)
-        data_df = fix_wkb_columns(data_df)
-        return data_df
+        Args:
+            schema_name: Name of the schema for the table from emannotationschemas
+            table_name: Table name for the database
+            model_name: Reference name for model (optional, defaults to same as table_name)
+            version: Materialization version (Optional)
+        """
+        if table_name in self._models:
+            return True
 
-    def query_cell_type(self, model_name, oids=None, cell_type=None):
-        if oids is None and cell_type is None:
-            filter_obj = None
-        elif oids is None:
-            if isinstance(cell_type, str):
-                cell_type = [cell_type]
-            filter_obj = self.models[model_name].cell_type.in_(cell_type)
-        elif cell_type is None:
-            filter_obj = self.models[model_name].pt_root_id.in_(oids)
-        else:
-            filter_obj = and_(self.models[model_name].pt_root_id.in_(oids),
-                              self.models[model_name].cell_type.in_(cell_type))
+        if "synapse" in table_name:
+            self._add_synapse_compartment_model(synapse_table_name=table_name)
 
-        data_df = self.run_query(model_name, filter_obj)
-        data_df = fix_wkb_columns(data_df)
-        return data_df
+        schema_name = get_annotation_info(self.dataset_name,
+                                          table_name)["schema_name"]
 
-    def query_cell_id(self, model_name, oids=None, cell_ids=None):
-        if isinstance(cell_ids, np.integer):
-            cell_ids = [cell_ids]
+        try:
+            self._models[table_name] = em_models.make_annotation_model(
+                dataset=self.dataset_name, annotation_type=schema_name,
+                table_name=table_name, version=self.materialization_version)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-        if oids is None and cell_ids is None:
-            filter_obj = None
-        elif oids is None:
-            filter_obj = self.models[model_name].func_id.in_(cell_ids)
-        elif cell_ids is None:
-            filter_obj = self.models[model_name].pt_root_id.in_(oids)
-        else:
-            filter_obj = and_(self.models[model_name].pt_root_id.in_(oids),
-                              self.models[model_name].func_id.in_(cell_ids))
 
-        data_df = self.run_query(model_name, filter_obj)
-        data_df = fix_wkb_columns(data_df)
-        return data_df
+    def _make_query(self, query_args, join_args=None, filter_args=None,
+                    select_columns=None):
+        """Constructs a query object with selects, joins, and filters
+
+        Args:
+            query_args: Iterable of objects to query
+            join_args: Iterable of objects to set as a join (optional)
+            filter_args: Iterable of iterables
+            select_columns: None or Iterable of str
+
+        Returns:
+            SQLAchemy query object
+        """
+
+        query = self.this_sqlalchemy_session.query(*query_args)
+
+        if join_args is not None:
+            query = query.join(*join_args)
+
+        if filter_args is not None:
+            for f in filter_args:
+                query = query.filter(*f)
+
+        if select_columns is not None:
+            query = query.with_entities(*select_columns)
+
+        return query
+
+    def _execute_query(self, query, fix_wkb=True, index_col=None):
+        """ Query the database and make a dataframe out of the results
+
+        Args:
+            query: SQLAlchemy query object
+            fix_wkb: Boolean to turn wkb objects into numpy arrays (optional, default is True)
+            index_col: None or str
+
+        Returns:
+            Dataframe with query results
+        """
+        df = pd.read_sql(query.statement, self.sqlalchemy_engine,
+                         coerce_float=False, index_col=index_col)
+        if fix_wkb:
+            df = fix_wkb_columns(df)
+
+        return df
+
+
+    def _query(self, query_args, join_args=None, filter_args=None,
+               select_columns=None, fix_wkb=True, index_col=None):
+        """ Wraps make_query and execute_query in one function
+
+        :param query_args:
+        :param join_args:
+        :param filter_args:
+        :param select_columns:
+        :param fix_wkb:
+        :param index_col:
+        :return:
+        """
+
+        query = self._make_query(query_args=query_args,
+                                 join_args=join_args,
+                                 filter_args=filter_args,
+                                 select_columns=select_columns)
+
+        df = self._execute_query(query=query, fix_wkb=fix_wkb,
+                                 index_col=index_col)
+
+        return df
