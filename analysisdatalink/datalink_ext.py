f5aa356f4b1e585b1c8a591a3f22d7ced489ff0f
Casey Schneider-Mizell <caseysm@gmail.com>
2019-10-09 15:07:11 -0700

    Improved docstrings

--- analysisdatalink/datalink_ext.py
+++ analysisdatalink/datalink_ext.py
@@ -16,23 +16,47 @@ class AnalysisDataLinkExt(datalink.AnalysisDataLink):
                        compartment_include_filter=None,
                        include_autapses=False,
                        compartment_table=None, return_sql=False,
-                       fix_wkb=True, fix_decimal=True, import_via_buffer=False,
+                       fix_wkb=True, fix_decimal=True, import_via_buffer=True,
                        n_threads=None):
-        """ Query synapses
-
-        :param synapse_table: str
-            table name without dataset prefix or version suffix
-        :param pre_ids: None, list or np.ndarray
-        :param post_ids: None, list or np.ndarray
-        :param compartment_table: None, str
-            defines compartment table -- has to be 'postsynapsecompartment'
-        :param compartment_include_filter: list of str
-        :param include_autapses: bool
-        :param compartment_table: None, str
-            DO NOT USE at the moment since there are no good compartment
-            labels yet
-        :return:
+        """Query a synapse table and return a dataframe
+        
+        Parameters
+        ----------
+        synapse_table : str
+            Table name with a synapse schema
+        pre_ids : collection of ints, optional
+            Object ids for presynaptic neurons, by default None
+        post_ids : collection of ints, optional
+            Object ids for postsynaptic neurons, by default None
+        compartment_include_filter : None, optional
+            Not currently implemented. By default None
+        include_autapses : bool, optional
+            Include synapses whose pre- and post-synaptic objects are the same, by default False
+        compartment_table : str, optional
+            Not currently implemented. Would be a table name for synapse compartments. By default None
+        return_sql : bool, optional
+            Return the sqlalchemy query object instead of the data itself, by default False
+        fix_wkb : bool, optional
+            Convert wkb-formatted spatial location columns to numpy 3-vectors. Setting to False
+            can be much faster, but spatial information is not easy to parse. These columns can be
+            parsed after the fact with analysisdatalink.fix_wkb_column. Optional, by default True
+        fix_decimal : bool, optional
+            Convert Decimal columns to ints. Not used if import_via_buffer is True. By default True
+        import_via_buffer : bool, optional
+            Flag to determine whether to use a fast csv and tempfile based SQL import (if True) or the pandas
+            native read_sql import (if False). If column formatting is odd, try setting to False. Optional, by default True.
+        n_threads : int or None, optional
+            Number of threads to use when parsing columns to convert wkb. Unused if fix_wkb is False. If set to 1,
+            multiprocessing is not used and slower numpy vectorization is used instead.
+            If None, uses the number of cpus available on the device. By default None
+        
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame representation of the query results.
         """
+        
+        
 
         filter_in_dict = defaultdict(dict)
         filter_equal_dict = defaultdict(dict)
@@ -63,16 +87,41 @@ class AnalysisDataLinkExt(datalink.AnalysisDataLink):
     def query_cell_types(self, cell_type_table, cell_type_include_filter=None,
                          cell_type_exclude_filter=None, return_only_ids=False,
                          exclude_zero_root_ids=False, fix_wkb=True, fix_decimal=True,
-                         return_sql=False, import_via_buffer=False, n_threads=None):
-        """ Query cell type tables
-
-        :param cell_type_table: str
-            table name without dataset prefix or version suffix
-        :param cell_type_include_filter: list of str
-        :param cell_type_exclude_filter: list of str
-        :param return_only_ids: bool
-        :param exclude_zero_root_ids: bool
-        :return: pandas DataFrame or numpy array
+                         return_sql=False, import_via_buffer=True, n_threads=None):
+        """Query a synapse table and return a dataframe
+        
+        Parameters
+        ----------
+        cell_type_table : str
+            Table name with a cell_type schema
+        cell_type_include_filter : collection of str, optional
+            Cell types to include
+        cell_type_exclude_filter : collection of str, optional
+            Cell types to exclude 
+        return_only_ids : bool, optional
+            Process to include only root ids matching the query.
+        exclude_zero_root_ids : bool, optional
+            Fitler out points with a null segmentation id. 
+        return_sql : bool, optional
+            Return the sqlalchemy query object instead of the data itself, by default False
+        fix_wkb : bool, optional
+            Convert wkb-formatted spatial location columns to numpy 3-vectors. Setting to False
+            can be much faster, but spatial information is not easy to parse. These columns can be
+            parsed after the fact with analysisdatalink.fix_wkb_column. Optional, by default True
+        fix_decimal : bool, optional
+            Convert Decimal columns to ints. Not used if import_via_buffer is True. By default True
+        import_via_buffer : bool, optional
+            Flag to determine whether to use a fast csv and tempfile based SQL import (if True) or the pandas
+            native read_sql import (if False). If column formatting is odd, try setting to False. Optional, by default True.
+        n_threads : int or None, optional
+            Number of threads to use when parsing columns to convert wkb. Unused if fix_wkb is False. If set to 1,
+            multiprocessing is not used and slower numpy vectorization is used instead.
+            If None, uses the number of cpus available on the device. By default None
+        
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame representation of the query results.
         """
 
         filter_in_dict = defaultdict(dict)
@@ -109,16 +158,42 @@ class AnalysisDataLinkExt(datalink.AnalysisDataLink):
     def query_cell_ids(self, cell_id_table, cell_id_filter=None,
                        cell_id_exclude_filter=None, return_only_ids=False,
                        exclude_zero_root_ids=False, fix_wkb=True, fix_decimal=True,
-                       return_sql=False, import_via_buffer=False, n_threads=None):
-        """ Query cell ids
+                       return_sql=False, import_via_buffer=True, n_threads=None):
 
-        :param cell_id_table: str
-            table name without dataset prefix or version suffix
-        :param cell_id_filter: list of uint64s
-        :param cell_id_exclude_filter: list of uint64s
-        :param return_only_ids: bool
-        :param exclude_zero_root_ids:bool
-        :return: pandas DataFrame or numpy array
+        """ Query cell id tables
+        
+        Parameters
+        ----------
+        cell_id_table : str
+            Table name for a microns_functional_coregistration table
+        cell_id_filter : list of uint64s, optional
+            List of root ids to include. Default is None.
+        cell_id_exclude_filter : list of uint64s, optional
+            List of root ids to exclude. Default is None.
+        return_only_ids : bool, optional
+            Process to include only root ids matching the query. Default is False.
+        exclude_zero_root_ids : bool, optional
+            Fitler out points with a null segmentation id. Default is False.
+        return_sql : bool, optional
+            Return the sqlalchemy query object instead of the data itself, by default False
+        fix_wkb : bool, optional
+            Convert wkb-formatted spatial location columns to numpy 3-vectors. Setting to False
+            can be much faster, but spatial information is not easy to parse. These columns can be
+            parsed after the fact with analysisdatalink.fix_wkb_column. Optional, by default True
+        fix_decimal : bool, optional
+            Convert Decimal columns to ints. Not used if import_via_buffer is True. By default True
+        import_via_buffer : bool, optional
+            Flag to determine whether to use a fast csv and tempfile based SQL import (if True) or the pandas
+            native read_sql import (if False). If column formatting is odd, try setting to False. Optional, by default True.
+        n_threads : int or None, optional
+            Number of threads to use when parsing columns to convert wkb. Unused if fix_wkb is False. If set to 1,
+            multiprocessing is not used and slower numpy vectorization is used instead.
+            If None, uses the number of cpus available on the device. By default None
+        
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame representation of the query results.
         """
         filter_in_dict = defaultdict(dict)
         if cell_id_filter is not None:
@@ -153,18 +228,41 @@ class AnalysisDataLinkExt(datalink.AnalysisDataLink):
                     cell_id_exclude_filter=None, return_only_mapping=False,
                     exclude_zero_root_ids=False,
                     fix_wkb=True, fix_decimal=True,
-                    return_sql=False, import_via_buffer=False, n_threads=None):
-        """ Queries coregistration
-
-        :param coreg_table: str
-            table name without dataset prefix or version suffix
-        :param cell_id_filter: list of uint64s
-        :param cell_id_exclude_filter: list of uint64s
-        :param return_only_mapping: bool
-            returns an array of [[root_id, f_id], ...]
-        :param exclude_zero_root_ids: bool
-            exclude zero root ids
-        :return: pandas DataFrame or numpy array
+                    return_sql=False, import_via_buffer=True, n_threads=None):
+        """ Query cell id tables
+        
+        Parameters
+        ----------
+        coreg_table : str
+            Table name for a microns_functional_coregistration table
+        cell_id_filter : list of uint64s, optional
+            List of root ids to include. Default is None.
+        cell_id_exclude_filter : list of uint64s, optional
+            List of root ids to exclude. Default is None.
+        return_only_ids : bool, optional
+            Process to include only root ids matching the query. Default is False.
+        exclude_zero_root_ids : bool, optional
+            Fitler out points with a null segmentation id. Default is False.
+        return_sql : bool, optional
+            Return the sqlalchemy query object instead of the data itself, by default False
+        fix_wkb : bool, optional
+            Convert wkb-formatted spatial location columns to numpy 3-vectors. Setting to False
+            can be much faster, but spatial information is not easy to parse. These columns can be
+            parsed after the fact with analysisdatalink.fix_wkb_column. Optional, by default True
+        fix_decimal : bool, optional
+            Convert Decimal columns to ints. Not used if import_via_buffer is True. By default True
+        import_via_buffer : bool, optional
+            Flag to determine whether to use a fast csv and tempfile based SQL import (if True) or the pandas
+            native read_sql import (if False). If column formatting is odd, try setting to False. Optional, by default True.
+        n_threads : int or None, optional
+            Number of threads to use when parsing columns to convert wkb. Unused if fix_wkb is False. If set to 1,
+            multiprocessing is not used and slower numpy vectorization is used instead.
+            If None, uses the number of cpus available on the device. By default None
+        
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame representation of the query results.
         """
         filter_in_dict = defaultdict(dict)
         if cell_id_filter is not None:
